a039634f44fa2fbeeffed2d111e4f727ce45fd4c
vitoffer <ms.titoff1011@yandex.ru>
2025-02-13 15:41:38 +0300

    add tailwind, primevue and cleaned project from unused components

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,24 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      cssLayer: {
+        name: 'primevue',
+        order: 'tailwind, primevue',
+      },
+      darkModeSelector: '.dark-theme',
+    },
+  },
+})
 
 app.mount('#app')
