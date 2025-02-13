177a89478f0b526de392026b5f16b11e43fc7d8c
vitoffer <ms.titoff1011@yandex.ru>
2025-02-13 18:06:47 +0300

    fix video links from base exercises list

--- src/base-data/exercises.ts
+++ src/base-data/exercises.ts
@@ -1,14 +1,4 @@
-interface Exercise {
-  name: string
-  videoUrl?: string | null
-  photoUrlList: string[]
-  description?: string | null
-  difficulty: Difficulty
-  sportsItems: string[]
-  tags: string[]
-}
-
-type Difficulty = 'простое' | 'среднее' | 'сложное'
+import type { Exercise } from '@/types'
 
 export default <Exercise[]>[
   {
@@ -22,7 +12,7 @@ export default <Exercise[]>[
   },
   {
     name: 'Упражнение 2',
-    videoUrl: 'https://rutube.ru/video/348d394918bfe218476abc144918f909/',
+    videoUrl: 'https://rutube.ru/play/embed/348d394918bfe218476abc144918f909',
     photoUrlList: [],
     description: null,
     difficulty: 'среднее',
@@ -43,7 +33,7 @@ export default <Exercise[]>[
   },
   {
     name: 'Крайний случай',
-    videoUrl: 'https://rutube.ru/video/348d394918bfe218476abc144918f909/',
+    videoUrl: 'https://rutube.ru/play/embed/348d394918bfe218476abc144918f909',
     photoUrlList: [
       'https://fitnessclub24.ru/public/images/gallery/section/thumbs/0/2-3-shragi.jpg',
       'https://avatars.dzeninfra.ru/get-zen_doc/1535103/pub_64dbaa4e73f157490e767716_64dbe7e2a9216a3544dafebe/scale_1200',
