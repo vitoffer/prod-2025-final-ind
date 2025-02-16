2036a59b1b2ce3c0e5395a355ac6b284d23d6783
vitoffer <ms.titoff1011@yandex.ru>
2025-02-17 00:58:54 +0300

    add exercises list to localstorage

--- src/stores/exercisesStore.ts
+++ src/stores/exercisesStore.ts
@@ -4,10 +4,18 @@ import baseExercisesList from '@/base-data/exercises'
 import type { Exercise } from '@/types'
 
 export const useExercisesStore = defineStore('exercises', () => {
-  const list = ref(baseExercisesList)
+  const list = ref<Exercise[]>([])
+  if (localStorage.getItem('exercisesList') === null) {
+    list.value = baseExercisesList
+    localStorage.setItem('exercisesList', JSON.stringify(list.value))
+  } else {
+    list.value = JSON.parse(localStorage.getItem('exercisesList')!)
+  }
 
   function removeExercise(id: number) {
     list.value = list.value.filter((exercise) => exercise.id !== id)
+
+    localStorage.setItem('exercisesList', JSON.stringify(list.value))
   }
 
   function updateExercise(id: number, updatedExercise: Exercise) {
@@ -15,10 +23,14 @@ export const useExercisesStore = defineStore('exercises', () => {
     if (index !== -1) {
       list.value[index] = { ...list.value[index], ...updatedExercise }
     }
+
+    localStorage.setItem('exercisesList', JSON.stringify(list.value))
   }
 
   function createExercise(newExercise: Exercise) {
     list.value.push(newExercise)
+
+    localStorage.setItem('exercisesList', JSON.stringify(list.value))
   }
 
   return { list, removeExercise, updateExercise, createExercise }
