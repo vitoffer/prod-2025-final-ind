8491d94fed364829837f399e96ef5102e703fc95
vitoffer <ms.titoff1011@yandex.ru>
2025-02-17 06:20:55 +0300

    add some workouts

--- src/base-data/workouts.ts
+++ src/base-data/workouts.ts
@@ -13,4 +13,49 @@ export default <Workout[]>[
       },
     ],
   },
+  {
+    id: 2,
+    name: 'Тренировка 2',
+    exercises: [
+      {
+        id: 2,
+        goal: {
+          repetitions: 20,
+          weight: 20,
+        },
+      },
+      {
+        id: 3,
+        goal: {
+          repetitions: 2,
+          weight: 10,
+        },
+      },
+      {
+        id: 4,
+        goal: {
+          time: 100,
+        },
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Тренировка 3',
+    exercises: [
+      {
+        id: 2,
+        goal: {
+          repetitions: 20,
+          weight: 100,
+        },
+      },
+      {
+        id: 4,
+        goal: {
+          time: 5,
+        },
+      },
+    ],
+  },
 ]
--- src/views/WorkoutsListPage.vue
+++ src/views/WorkoutsListPage.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useWorkoutsStore } from '@/stores/workoutsStore'
 
-<template>workouts</template>
+const workoutsList = useWorkoutsStore()
+</script>
+
+<template>
+  <div v-for="workout in workoutsList.list" :key="workout.id">
+    {{ workout.name }}
+  </div>
+</template>
 
 <style scoped></style>
