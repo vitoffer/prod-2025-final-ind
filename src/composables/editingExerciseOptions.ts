c6897c8dbb8b16ebc881496d2c2235c94403bf2d
vitoffer <ms.titoff1011@yandex.ru>
2025-02-17 06:06:01 +0300

    add possibility to create new tags or sportsItems in edit exercise dialog

--- src/composables/editingExerciseOptions.ts
+++ src/composables/editingExerciseOptions.ts
@@ -9,12 +9,19 @@ export function useEditingExerciseOptions() {
     sportsItemsSelectEditingSuggestions.value = sportsItemsOptions.value
       .filter((option) => option.name.toLowerCase().includes(event.query.toLowerCase()))
       .map((option) => option.name)
+    if (sportsItemsSelectEditingSuggestions.value.length === 0) {
+      sportsItemsSelectEditingSuggestions.value.push(event.query.toLowerCase())
+    }
   }
 
   const searchTagsSelectEditing = (event: AutoCompleteCompleteEvent) => {
     tagsSelectEditingSuggestions.value = tagsOptions.value
       .filter((option) => option.name.toLowerCase().includes(event.query.toLowerCase()))
       .map((option) => option.name)
+
+    if (tagsSelectEditingSuggestions.value.length === 0) {
+      tagsSelectEditingSuggestions.value.push(event.query.toLowerCase())
+    }
   }
 
   const sportsItemsSelectEditingSuggestions = ref<string[]>([])
