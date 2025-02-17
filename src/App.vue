ce35336aa198b7c5731d0e1cab2495a71001db73
vitoffer <ms.titoff1011@yandex.ru>
2025-02-17 23:13:53 +0300

    add navigation to every page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
+  <nav>
+    <ul class="flex justify-center gap-4">
+      <li>
+        <Button severity="info">
+          <RouterLink :to="{ name: 'ExercisesListPage' }">Список упражнений</RouterLink>
+        </Button>
+      </li>
+      <li>
+        <Button severity="info">
+          <RouterLink :to="{ name: 'WorkoutsListPage' }">Список тренировок</RouterLink>
+        </Button>
+      </li>
+      <li>
+        <Button severity="info">
+          <RouterLink :to="{ name: 'ProfilePage' }">Профиль</RouterLink>
+        </Button>
+      </li>
+    </ul>
+  </nav>
   <RouterView></RouterView>
 </template>
 
