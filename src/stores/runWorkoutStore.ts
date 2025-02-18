6ebeae01fe400ff80d25b06a93fe41fc907086cd
vitoffer <ms.titoff1011@yandex.ru>
2025-02-18 04:11:50 +0300

    add some info to run workout page

--- src/stores/runWorkoutStore.ts
+++ src/stores/runWorkoutStore.ts
@@ -1,9 +1,12 @@
 import type { Workout } from '@/types'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { useWorkoutsStore } from './workoutsStore'
 
 export const useRunWorkoutStore = defineStore('runWorkout', () => {
-  const selectedRunWorkout = ref<Workout | null>(null)
+  // const selectedRunWorkout = ref<Workout | null>(null)
+  const workoutsStore = useWorkoutsStore()
+  const selectedRunWorkout = ref<Workout | null>(workoutsStore.list[1])
 
   function changeRunWorkout(workout: Workout) {
     selectedRunWorkout.value = workout
--- src/views/RunWorkoutPage.vue
+++ src/views/RunWorkoutPage.vue
@@ -1,11 +1,73 @@
 <script setup lang="ts">
+import ExerciseCardInfo from '@/components/ExerciseCardInfo.vue'
 import { useRunWorkoutStore } from '@/stores/runWorkoutStore'
+import type { ExerciseWithGoal } from '@/types'
+import { computed, ref, watch, watchEffect } from 'vue'
 
 const runWorkoutStore = useRunWorkoutStore()
+
+const currentExerciseIndex = ref<number>(0)
+const currentExercise = computed<ExerciseWithGoal>(() => {
+  return runWorkoutStore.selectedRunWorkout!.exercises[currentExerciseIndex.value]
+})
+
+const workoutCompleted = ref<boolean>(false)
+
+const completeExercise = () => {
+  if (currentExerciseIndex.value === runWorkoutStore.selectedRunWorkout!.exercises.length - 1) {
+    workoutCompleted.value = true
+    elapsedWorkoutTime.value = new Date().getTime() - startWorkoutTime
+  } else {
+    currentExerciseIndex.value++
+  }
+}
+
+const startWorkoutTime = new Date().getTime()
+const elapsedWorkoutTime = ref<number | null>(null)
+
+const startExerciseTime = ref<number | null>(null)
+const endExerciseTime = ref<number | null>(null)
+
+const exercisesListInfo = ref<any>([])
+
+watch(
+  () => currentExercise.value,
+  () => {
+    if (startExerciseTime.value) {
+      exercisesListInfo.value[currentExerciseIndex.value] =
+        new Date().getTime() - startExerciseTime.value
+    }
+    if (currentExercise.value.units.includes('мин')) {
+      startExerciseTime.value = new Date().getTime()
+    } else {
+      startExerciseTime.value = null
+    }
+  },
+  { immediate: true },
+)
+
+setTimeout(() => {
+  completeExercise()
+  setTimeout(() => {
+    completeExercise()
+    // setTimeout(completeExercise, 0)
+  }, 2000)
+}, 1000)
 </script>
 
 <template>
-  <pre>{{ JSON.stringify(runWorkoutStore.selectedRunWorkout, null, 2) }}</pre>
+  <p>{{ currentExercise.name }}</p>
+  <div class="wrapper h-[250px] w-[450px]">
+    <ExerciseCardInfo :exercise="currentExercise" />
+  </div>
+  <div v-if="currentExercise.units.includes('мин')" class="timer">
+    Затраченное время на упражнение:
+    {{ Math.ceil((new Date().getTime() - startExerciseTime!) / 1000) }} секунд
+  </div>
+  <div v-if="!currentExercise.units.includes('мин')">
+    <Button severity="success" @click="completeExercise">Готово</Button>
+  </div>
+  <p>{{ workoutCompleted ? elapsedWorkoutTime : '' }}</p>
 </template>
 
 <style scoped></style>
