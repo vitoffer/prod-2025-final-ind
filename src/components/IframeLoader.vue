4e66943e82ecda21d831a9becb1d4eadce45b83d
vitoffer <ms.titoff1011@yandex.ru>
2025-02-18 21:17:34 +0300

    add iframe load timeout

--- src/components/IframeLoader.vue
+++ src/components/IframeLoader.vue
@@ -1,28 +1,46 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, useTemplateRef } from 'vue'
 
 defineProps<{ src: string }>()
 
-const isLoaded = ref<boolean>(false)
-const hasError = ref<boolean>(false)
+const isLoaded = ref(false)
+const hasError = ref(false)
 
 const handleIframeLoad = async () => {
+  if (!firstLoad.value) return
+
+  clearTimeout(timeoutId.value)
+
   isLoaded.value = true
   hasError.value = false
 }
 
 const handleIframeError = () => {
+  clearTimeout(timeoutId.value)
+
   isLoaded.value = false
   hasError.value = true
 }
+
+const iframe = useTemplateRef('iframe')
+const firstLoad = ref(true)
+const timeoutId = ref(
+  setTimeout(() => {
+    isLoaded.value = false
+    hasError.value = true
+    if (!iframe.value) return
+    firstLoad.value = false
+    iframe.value.src = ''
+  }, 3000),
+)
 </script>
 
 <template>
   <div class="flex items-center justify-center">
-    <div v-if="hasError">
+    <p v-if="hasError" class="text-center">
       Ошибка загрузки контента. Пожалуйста, проверьте ссылку или попробуйте позже.
-    </div>
-    <div v-else-if="!isLoaded">Загрузка...</div>
+    </p>
+    <p v-else-if="!isLoaded" class="text-center">Загрузка...</p>
     <iframe
       v-show="isLoaded && !hasError"
       :src="src"
@@ -31,6 +49,7 @@ const handleIframeError = () => {
       allowfullscreen
       title="Видео плеер"
       :class="$attrs.class"
+      ref="iframe"
     />
   </div>
 </template>
