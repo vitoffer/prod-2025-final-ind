f8274cc5a5da9f4dc570e56f20ce70980fac29e2
vitoffer <ms.titoff1011@yandex.ru>
2025-02-19 00:27:13 +0300

    start styling navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav>
-    <ul class="flex justify-center gap-4">
-      <li>
-        <Button severity="info">
-          <RouterLink :to="{ name: 'ExercisesListPage' }">Список упражнений</RouterLink>
-        </Button>
-      </li>
-      <li>
-        <Button severity="info">
-          <RouterLink :to="{ name: 'WorkoutsListPage' }">Список тренировок</RouterLink>
-        </Button>
-      </li>
-      <li>
-        <Button severity="info">
-          <RouterLink :to="{ name: 'ProfilePage' }">Профиль</RouterLink>
-        </Button>
-      </li>
-    </ul>
-  </nav>
-  <RouterView></RouterView>
+  <div class="relative flex flex-col">
+    <nav
+      class="sticky bottom-0 z-10 order-1 justify-self-end border-t border-t-gray-600 bg-[#121212] p-2 sm:static sm:border-none"
+    >
+      <ul class="flex justify-center gap-4">
+        <li>
+          <Button severity="info" class="!pt-1 !pb-1">
+            <RouterLink :to="{ name: 'ExercisesListPage' }" class="">Упражнения</RouterLink>
+          </Button>
+        </li>
+        <li>
+          <Button severity="info" class="!pt-1 !pb-1">
+            <RouterLink :to="{ name: 'WorkoutsListPage' }" class="">Тренировки</RouterLink>
+          </Button>
+        </li>
+        <li>
+          <Button severity="info" class="!pt-1 !pb-1">
+            <RouterLink :to="{ name: 'ProfilePage' }" class="">Профиль</RouterLink>
+          </Button>
+        </li>
+      </ul>
+    </nav>
+    <div class="sm:order-1">
+      <RouterView></RouterView>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
